d402254dc121a5bd6080c47011ecc0fb9cf05e5a
baruch <baruchminzberg@gmail.com>
2025-06-08 21:37:36 +0300

    Add project files.

---
+++ ConsoleApp4/DAL.cs
@@ -0,0 +1,107 @@
+﻿
+using c__SQL.Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace c__SQL.DAL
+{
+    internal class DAL
+    {
+        Agent Agent;
+        private string connStr = "server=127.0.0.1;user=root;password=;database=agents";
+        private MySqlConnection _conn;
+
+        public MySqlConnection openConnection()
+        {
+            if (_conn == null)
+            {
+                _conn = new MySqlConnection(connStr);
+            }
+
+            if (_conn.State != System.Data.ConnectionState.Open)
+            {
+                _conn.Open();
+                Console.WriteLine("Connection successful.");
+            }
+
+            return _conn;
+        }
+
+        public void closeConnection()
+        {
+            if (_conn != null && _conn.State == System.Data.ConnectionState.Open)
+            {
+                _conn.Close();
+                _conn = null;
+            }
+        }
+
+        public DAL()
+        {
+            try
+            {
+                openConnection();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"MySQL Error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"General Error: {ex.Message}");
+            }
+        }
+
+        public List<Agent> GetAgents(string query = "SELECT * FROM agents")
+        {
+            List<Agent> agents = new List<Agent>();
+            MySqlCommand cmd = null;
+            MySqlDataReader reader = null;
+            try
+            {
+                openConnection();
+                cmd = new MySqlCommand(query, _conn);
+                reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32("id");
+                    string odeName = reader.GetString("odeName");
+                    string realName = reader.GetString("realName");
+                    string location = reader.GetString("location");
+                    string status = reader.GetString("status");
+                    int missionsCompleted = reader.GetInt32("missionsCompleted");
+                    Agent age = new Agent(id, odeName, realName, location, status, missionsCompleted);
+                    agents.Add(age);
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"Error while fetching agents: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while fetching agents: {ex.Message}");
+            }
+
+            finally
+            {
+                if (reader != null && !reader.IsClosed)
+                    reader.Close();
+
+                closeConnection();
+            }
+
+            return agents;
+        }
+
+        public void printAgentList(List<Agent> agents)
+        {
+            foreach (Agent agent in agents)
+            {
+                Console.WriteLine(agent);
+            }
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using c__SQL.DAL;
+
+using MySql.Data.MySqlClient;
+
+
+namespace c__SQL.DAL
+{
+
+    internal class Program
+    {
+        static void Main()
+        {
+            DAL dal = new DAL();
+            dal.GetAgents();
+        }
+    }
+}
